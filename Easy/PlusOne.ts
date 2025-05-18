bb964d75c84951a988efa2496e427fc6b008eb57
Jonatan Helander <Jonatanhelander@hotmail.com>
2025-05-18 21:23:36 +0200

    difficult one

---
+++ Easy/PlusOne.ts
@@ -0,0 +1,20 @@
+// You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+// Increment the large integer by one and return the resulting array of digits.
+function plusOne(digits: number[]): number[] {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+
+    digits[i] = 0;
+  }
+  return [1, ...digits];
+}
+
+const input = [9, 1, 9, 2, 9, 9];
+// const input = [9, 9];
+// const input = [6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3];
+
+plusOne(input);
